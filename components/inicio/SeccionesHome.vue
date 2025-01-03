ee5fbdd16ec4c8a1f28c887989f6c13fc333909e
ComputoEspacial <computoespacial@gmail.com>
2025-01-03 09:51:23 -0600

    fixed home

--- components/inicio/SeccionesHome.vue
+++ components/inicio/SeccionesHome.vue
@@ -102,7 +102,7 @@ const sections = ref([
         title: 'DIF Municipal',
         description: 'Servicios y programas de asistencia social',
         image: '/images/iconos_secciones/Dif_guindo.png',
-        link: '/dif-municipal'
+        link: '/sections/dif-municipal'
     },
     {
         title: 'Paga Tu Predial',
@@ -114,19 +114,19 @@ const sections = ref([
         title: 'Dispensario Medico',
         description: 'Atención médica accesible',
         image: '/images/iconos_secciones/Dispensario_medico_guindo.png',
-        link: '/dispensario-medico'
+        link: '/sections/dispensario-medico'
     },
     {
         title: 'Obras y Servicios Públicos',
         description: 'Información sobre obras y servicios en tu ciudad',
         image: '/images/iconos_secciones/Obras_publicas_guindo.png',
-        link: '/obras-servicios-publicos'
+        link: '/sections/obras-servicios-publicos'
     },
     {
         title: 'Ver Puentes Internacionales',
         description: 'Estado y tiempos de espera en cruces fronterizos',
         image: '/images/iconos_secciones/Puentes_guindo.png',
-        link: '/puentes-internacionales'
+        link: '/sections/puentes-internacionales'
     }
 ])
 
