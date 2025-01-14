60e5fdbe30b09f67290761bb4037540ca06cae54
ComputoEspacial <computoespacial@gmail.com>
2025-01-14 11:35:33 -0600

    icon shit

---
+++ components/inicio/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="icon-wrapper">
+    <Icon :name="name" v-bind="$attrs" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
