93768ae1c878c2099b970739e6f61553b0c3a334
ComputoEspacial <computoespacial@gmail.com>
2025-01-24 14:47:21 -0600

    correo

--- app/config.ts
+++ app/config.ts
@@ -4,5 +4,12 @@ export default defineNuxtConfig({
     head: {
       title: 'Piedras Negras'
     }
+  },
+  runtimeConfig: {
+    emailUser: process.env.EMAIL_USER,
+    emailPassword: process.env.EMAIL_PASSWORD,
+    public: {
+      formSubmissionEmail: 'contraloria@piedrasnegras.gob.mx'
+    }
   }
 })
